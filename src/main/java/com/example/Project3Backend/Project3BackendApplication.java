6b1484b750c7f41137304ea90ac8b6e076dcb270
sbagos27 <sbagos@csumb.edu>
2025-10-23 13:22:39 -0700

    Added springboot and can run locally

---
+++ src/main/java/com/example/Project3Backend/Project3BackendApplication.java
@@ -0,0 +1,13 @@
+// package com.example.Project3Backend;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class Project3BackendApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(Project3BackendApplication.class, args);
+// 	}
+
+// }
