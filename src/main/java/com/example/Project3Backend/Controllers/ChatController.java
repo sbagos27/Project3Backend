8d9ea4e6ca31d17b6327e31e7d1c33c2c7d6e8af
gabethemyers <gabethemyers@gmail.com>
2025-11-07 15:53:52 -0800

    db-check working now maybe

--- src/main/java/com/example/Project3Backend/Controllers/ChatController.java
+++ src/main/java/com/example/Project3Backend/Controllers/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.RestController;
 
